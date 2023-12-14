7f122e4bd3335312b25b197132e2f66d8109704a
MSL <manuel.sierra@bluetab.net>
2023-12-14 13:57:33 +0100

    feat: update prompt

--- llm_api/api/services/validator.py
+++ llm_api/api/services/validator.py
@@ -10,7 +10,7 @@ class Validator:
         content = [
             {
                 "type": "text",
-                "text": "La siguiente imagen es el diseño master."
+                "text": "La siguiente imagen es el diseño master de una campaña publicitaria que se va a usar como referencia para validar una nueva imagen que es una variación de la imagen master adaptada para algún medio como pueden ser banners, redes sociales, cartelería, etc"
             },
             {
                 "type": "image_url",
@@ -21,24 +21,27 @@ class Validator:
             },
             {
                 "type": "text",
-                "text": """Estos son los criterios de validacion: 
-                    - Márgenes simetricos.
-                    - Buen contraste entre los elementos y el fondo.
-                    - Legibilidad de los textos.
-                    - Inexistencia de redundancia entre los elementos.
-                    - Los logotipos no se pueden repetir.
-                    - Los Call To Action (CTA) no se pueden repetir.
-                    - El texto de los Call To Action (CTA) debe ser consistente en mayusculas y minusculas.
-                    - Conservación la tipografía de los mensajes clave del diseño master.
-                    - Priorización mensajes clave y no saturar con contenido redundante.
+                "text": """Tienes que analizar y validar una series de criterios: 
+                    - 'margenes': Loa margenes de la imagen deben ser simétricos, es decir que haya el mismo espacio entre el contenido de la imagen y los bordes. El resultado deberías ser 'Apto' o 'No apto'. En el caso de 'No apto' incluir una breve justificación.
+                    - 'contraste': Buen contraste entre los elementos de la imagen y el fondo. El resultado deberías ser 'Apto' o 'No apto'. En el caso de 'No apto' incluir una breve justificación.
+                    - 'legibilidad': Legibilidad de los textos. El resultado deberías ser 'Apto' o 'No apto'. En el caso de 'No apto' incluir una breve justificación.
+                    - 'redundancia': Inexistencia de redundancia entre los elementos. El resultado deberías ser 'Apto' o 'No apto'. En el caso de 'No apto' incluir una breve justificación.
+                    - 'logos_rep': Los logotipos no se pueden repetir. El resultado deberías ser 'Apto' o 'No apto'. En el caso de 'No apto' incluir una breve justificación.
+                    - 'cta': Los Call To Action (CTA) no se pueden repetir. El resultado deberías ser 'Apto' o 'No apto'. En el caso de 'No apto' incluir una breve justificación.
+                    - 'cta_text': El texto de los Call To Action (CTA) debe ser consistente en mayusculas y minusculas. El resultado deberías ser 'Apto' o 'No apto'. En el caso de 'No apto' incluir una breve justificación.
+                    - 'tipografia': Conservación la tipografía de los mensajes clave del diseño master. El resultado deberías ser 'Apto' o 'No apto'. En el caso de 'No apto' incluir una breve justificación.
+                    - 'mensaje': Priorización mensajes clave y no saturar con contenido redundante. El resultado deberías ser 'Apto' o 'No apto'. En el caso de 'No apto' incluir una breve justificación.
                 """
             },
             {
                 "type": "text",
-                "text": """Valida la siguiente imagen de forma exhaustiva tomando como referencia el diseño master.
-                    Importante! Tu respuesta debe ser en español en un objeto raw JSON sin formatear con la siguiente estructura::
+                "text": """Valida la siguiente imagen de forma exhaustiva tomando como referencia el diseño master para algunos de los criterios mencionados.
+                    Importante! Tu respuesta debe ser en español en un objeto raw JSON sin formatear (Importante! No incluir ```json) con la siguiente estructura:
                     - Clave: 'validate', Valor: Porcentaje de validez de la adaptación en decimales.
-                    - Clave: 'errores', Valor: resumen de los errores encontrados en formato string.
+                    - Clave: 'errores', Valor: resumen de las validaciones. Debe contener una lista con cada uno de los criterios a analizar.
+                    ['margenes', 'contraste', 'legibilidad', 'redundancia', 'logos_rep', 'cta', 'cta_text', 'tipografia', 'mensaje']
+                    Por ejemplo:
+                    "errores": [{"margenes": "No apto. El margen derecho es mucho mayor al izquierdo"}, {"contraste": "Apto"}, {"tipografia": "No Apto. La tipografía de la variación con coincide con la de la imagen master"}]
                 """
             },
             {
